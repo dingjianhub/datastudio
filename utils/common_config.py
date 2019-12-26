37c07225e0823bb902498844460e0908893e7507
fangtaoa <15026687761@163.com>
2019-12-26 22:37:49 +0800

    20191226更新

--- utils/common_config.py
+++ utils/common_config.py
@@ -1,6 +1,6 @@
 class Config(object):
     def __init__(self):
-        self.sign = "15771929690468CA51394CAFC5FA7D0B5B37632E1B3B1"
+        self.sign = "15773680567038A56F075DDD6E4E222A2657F930A5793"
         self.token = "70f12f2f4f091e459a279469fe49eca5"
 
     def get_sign(self):
